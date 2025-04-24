bf4b55c9cf66eaffc4867f771a4842db0951c5c9
DayGTT <DGioTT.DG@upn.pe>
2025-04-24 11:41:53 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //fiebre,dolor de caveza,hipoglusemia=covid
+            //fiebre, dolor de caveza = infeccion
+            //fiebre= resfriado
+            Console.WriteLine("tienes fiebre (si/no): ");
+            string respuesta = Console.ReadLine();
+            if (respuesta == "si")
+            {
+                Console.WriteLine("tienes dolor de aveza? (si /no): ");
+                string respuesta2 = Console.ReadLine();
+                if (respuesta2 == "si")
+
+                {
+                    Console.WriteLine("tienes hipoglusemia?(si /no): ");
+                    string respuesta3 = Console.ReadLine();
+                    
+                        if (respuesta3 == "si ")
+                    {
+                        Console.WriteLine("usted es candidato a covid");
+                    }
+
+
+                    else 
+                    {
+                        Console.WriteLine("usted tiene un infeccion");
+                    }
+
+
+
+
+
+
+                }
+                else { Console.WriteLine("usted solo tiene una infeccion"); }
+
+
+            }
+            else { Console.WriteLine("usted solo tiene una infeccion"); }
+        }
+    }
+}
+                   
+                    
+    
+
